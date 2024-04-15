521cddcd81f2568d66cd9055d28302af0b675208
yangqi7 <yangqi7@vipkid.com.cn>
2024-04-15 10:56:09 +0800

    feat：新的修改

--- helloflutter/lib/02/07_单子布局组件.dart
+++ helloflutter/lib/02/07_单子布局组件.dart
@@ -27,23 +27,23 @@ class MyHome extends StatelessWidget {
 Container+BoxDecoration 圆角图片
 */
 
-// class MyBody extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Container(
-//         width: 200,
-//         height: 200,
-//         decoration: BoxDecoration(
-//           borderRadius: BorderRadius.circular(20),
-//           image: DecorationImage(
-//               image: NetworkImage(
-//                   "https://pic4.zhimg.com/v2-cb75f239dfd0c1c42c23dfc9011965a3_b.jpg")),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class MyBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+              image: NetworkImage(
+                  "https://pic4.zhimg.com/v2-cb75f239dfd0c1c42c23dfc9011965a3_b.jpg")),
+        ),
+      ),
+    );
+  }
+}
 /*
 Container+BoxDecoration
 */
@@ -89,23 +89,22 @@ Container+BoxDecoration
 // Padding
 // */
 
-class MyBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.lightGreen,
-      child: Padding(
-        padding: EdgeInsets.all(20),
-        child: Container(
-          decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-          child: Text(
-              "风急天高猿啸哀，渚清沙白鸟飞回。无边落木萧萧下，不尽长江滚滚来。万里悲秋常作客，百年多病独登台。艰难苦恨繁霜鬓，潦倒新停浊酒杯。"),
-        ),
-      ),
-    );
-  }
-}  
+// class MyBody extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Colors.lightGreen,
+//       child: Padding(
+//         padding: EdgeInsets.all(20),
+//         child: Container(
+//           decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+//           child: Text(
+//               "风急天高猿啸哀，渚清沙白鸟飞回。无边落木萧萧下，不尽长江滚滚来。万里悲秋常作客，百年多病独登台。艰难苦恨繁霜鬓，潦倒新停浊酒杯。"),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 /*
 Center
@@ -130,6 +129,7 @@ Center
 //     );
 //   }
 // }
+
 
 /*
 Align
